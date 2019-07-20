57941cc63a0279c88ca0274ef9227c4d2b401b0e
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-07-20 13:27:31 -0700

    small change to model

--- server/src/db/models/user.js
+++ server/src/db/models/user.js
@@ -1,8 +1,17 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var User = sequelize.define('User', {
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: {msg: 'must be valid email'}
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   }, {});
   User.associate = function(models) {
     // associations can be defined here
