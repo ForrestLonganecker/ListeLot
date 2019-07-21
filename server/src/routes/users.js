f350d9871bb507a9e028b677e2d471184f3c763f
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-07-21 16:10:34 -0700

    adding log in route

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.post('/users/create', userController.create);
+router.post('/users/logIn', userController.logIn);
 
 module.exports = router;
