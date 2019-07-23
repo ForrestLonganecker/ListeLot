47fcbda2b08b9d0a8e0764bde7df2fd17136cceb
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-07-22 18:36:15 -0700

    forms in place for log-in and sign-up

--- frontend/src/components/LogIn/LogIn.js
+++ frontend/src/components/LogIn/LogIn.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
 
 import './LogIn.css';
 
 const LogIn = ({ setActiveView }) => {
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+
   return(
-    <div>
+    <div className="log-in-container">
       <h1>Log in!</h1>
-      <button type="button" onClick={() => setActiveView('sign up')}>Sign up</button>
+      <form className="log-in-form">
+        <input type="text" placeholder="Enter email address" onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" placeholder="Enter password" onChange={(e) => setPassword(e.target.value)} />
+
+        <div className="button-container">
+        <button type="submit">Log in</button>
+        <button type="button" onClick={() => setActiveView('sign up')}>Sign up?</button>
+        </div>
+      </form>
     </div>
   );
 };
--- frontend/src/components/SignUp/SignUp.js
+++ frontend/src/components/SignUp/SignUp.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { useState } from 'react';
 
 import './SignUp.css';
 
 const SignUp = ({ setActiveView }) => {
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+
+
+
   return(
-    <div>
+    <div className="sign-up-container">
       <h1>Sign up!</h1>
-      <button type="button" onClick={() => setActiveView('log in')}>Log in</button>
+      <form className="sign-up-form">
+        <input type="text" placeholder="Enter email address" onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" placeholder="Enter password" onChange={(e) => setPassword(e.target.value)} />
+        <input type="password" placeholder="Confirm password" onChange={(e) => setPasswordConfirmation(e.target.value)} />
+
+        <div className="button-container">
+        <button type="submit">Log in</button>
+        <button type="button" onClick={() => setActiveView('log in')}>Log in?</button>
+        </div>
+      </form>
     </div>
   );
 };
