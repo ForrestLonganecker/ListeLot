1cf2fb2e8faf54be02004155846422fbcf10e56b
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-07-23 18:32:39 -0700

    added port normalization

--- server/src/server.js
+++ server/src/server.js
@@ -2,11 +2,22 @@ require('dotenv').config();
 const app = require('./app');
 const http = require('http');
 
-const port = process.env.PORT || 4000;
+const port = normalizePort(process.env.PORT || '4000');
 
 const server = http.createServer(app);
 
 server.listen(port);
+
+function normalizePort(val){
+  const port = parseInt(val, 10);
+  if(isNaN(port)){
+    return val;
+  }
+  if(port >= 0){
+    return port;
+  }
+  return false;
+}
 
 server.on('listening', () => {
   console.log(`ListeLot listening on port ${port}`);  
