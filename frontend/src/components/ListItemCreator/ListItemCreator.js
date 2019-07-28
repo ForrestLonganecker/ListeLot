04f33e23eb99704b04c2e0f4781661e84af9b716
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-07-27 21:53:20 -0700

    create item working

--- frontend/src/components/ListItemCreator/ListItemCreator.js
+++ frontend/src/components/ListItemCreator/ListItemCreator.js
@@ -1,13 +1,46 @@
 import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
 import './ListItemCreator.css';
 
+let createItemUrl;
+if(process.env.NODE_ENV === 'production'){
+  createItemUrl = 'https://listelot.herokuapp.com/listItems/create'
+}
+if(process.env.NODE_ENV === 'development'){
+  createItemUrl = 'http://localhost:4000/listItems/create'; 
+}
+
 const ListItemCreator = ({ listTitle, listId }) => {
 
+  const [text, setText] = useState('');
+
+  const handleCreateItem = (e) => {
+    e.preventDefault();
+
+    if(text.length > 25 || text.length < 1){
+      alert(`Adjust text length: ${text.length}. Must be 1-25 characters`)
+    } else {
+      let data = {
+        text: text,
+        listId: listId
+      };
+
+      axios.post(createItemUrl, data)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    };
+  }
+
   return(
-    <form  className="list-creator-form">
+    <form onSubmit={handleCreateItem} className="list-creator-form">
       <label className="create-list-label">{listTitle}</label>
-      <input className="create-list-input" type="text" placeholder="add a new item to your list" />
+      <input className="create-list-input" type="text" placeholder="add a new item to your list" onChange={(e) => setText(e.target.value)} />
       <button className="create-list-button" type="submit">Submit</button>
     </form>
   );
