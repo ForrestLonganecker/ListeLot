fb3ba80a3a0a2cc7c37b242fbbd7c7447ae2971e
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-08-25 13:49:35 -0700

    auth helpers converted to ts

--- server/src/helpers/auth.ts
+++ server/src/helpers/auth.ts
@@ -1,17 +1,25 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+interface Req {
+  headers: Headers
+}
+
+interface Headers {
+  authorization: string
+}
+
 module.exports = {
-  encryptPassword(inputPassword){
+  encryptPassword(inputPassword: string){
     const salt = bcrypt.genSaltSync();
     const hashedPassword = bcrypt.hashSync(inputPassword, salt);
     return hashedPassword;
   },
-  comparePasswords(inputPassword, databasePassword){
+  comparePasswords(inputPassword: string, databasePassword: string){
     // compare input and database values
     return bcrypt.compareSync(inputPassword, databasePassword);
   },
-  createToken(email){
+  createToken(email: string){
     const token = jwt.sign(
       { email: email },
       process.env.JWT_SECRET,
@@ -19,7 +27,7 @@ module.exports = {
     );
     return token;
   },
-  decode(token){
+  decode(token: Object){
     // checks jwt token for secret
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -30,7 +38,7 @@ module.exports = {
       return false;
     };
   },
-  hasToken(req){
+  hasToken(req: Req){
     if(req.headers['authorization']){
       // parse the token from the auth.string "Bearer ASgdASgAeGshT.uhuioSeGewvaVR.DfyTgerwe"
       let token = req.headers['authorization'].split(' ')[1];
